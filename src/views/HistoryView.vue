fc8e61ff34c2798c4fe91f9983e3091f90fc6a82
mevlanaayas <mevlanaayas@gmail.com>
2022-11-02 00:20:19 +0300

    filters and txn links

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -7,6 +7,9 @@ export default {
   data() {
     return {
       info: null,
+      networkFilters: [],
+      tagFilters: [],
+      typeFilters: [],
       colorMapping: {
         "social": '#63be2f',
         "transaction": '#e1ba1d',
@@ -30,28 +33,81 @@ export default {
       return Number(roundedString)
     },
     addressFormatter(row, column) {
-      return row[column.property] === undefined ? "No address" : row[column.property].slice(0, 7) + "..." +row[column.property].slice(row[column.property].length - 5)
+      return row[column.property] === undefined ? "No address" : row[column.property].slice(0, 7) + "..." + row[column.property].slice(row[column.property].length - 5)
     },
     toUpperCase(str) {
       return str.charAt(0).toUpperCase() + str.slice(1)
+    },
+    filterTag(value, row) {
+      return row.tag === value
+    },
+    filterType(value, row) {
+      return row.type === value
+    },
+    filterNetwork(value, row) {
+      return row.network === value
+    },
+    refresh() {
+      this.getHistory();
+    },
+    getDomain(url) {
+      let domain = (new URL(url));
+      return this.toUpperCase(domain.host.split(".")[0])
+    },
+    getHistory() {
+      let params = {
+        "address": [
+          this.address
+        ],
+        "limit": 500,
+        "refresh": false,
+        "include_poap": false,
+        "ignore_contract": false,
+        "count_only": false,
+        "query_status": false
+      }
+      Api.GetNotes(params)
+          .then(response => {
+            this.info = response.data.result
+            const tags = new Set();
+            for (let i = 0; i < response.data.result.length; i++) {
+              tags.add(response.data.result[i].tag);
+            }
+            const tagFilters = [];
+            let self = this
+            tags.forEach(function (value) {
+              tagFilters.push({"text": self.toUpperCase(value), "value": value});
+            })
+            this.tagFilters = tagFilters
+
+            const types = new Set();
+            for (let i = 0; i < response.data.result.length; i++) {
+              types.add(response.data.result[i].type);
+            }
+            const typeFilters = [];
+            types.forEach(function (value) {
+              typeFilters.push({"text": self.toUpperCase(value), "value": value});
+            })
+            this.typeFilters = typeFilters
+
+            const networks = new Set();
+            for (let i = 0; i < response.data.result.length; i++) {
+              networks.add(response.data.result[i].network);
+            }
+            const networkFilters = [];
+            networks.forEach(function (value) {
+              networkFilters.push({"text": self.toUpperCase(value), "value": value});
+            })
+            this.networkFilters = networkFilters
+          })
     }
   },
   mounted() {
-    let params = {
-      "address": [
-        this.address
-      ],
-      "limit": 500,
-      "refresh": false,
-      "include_poap": false,
-      "ignore_contract": false,
-      "count_only": false,
-      "query_status": false
-    }
-    Api.GetNotes(params)
-        .then(response => {
-          this.info = response.data.result
-        })
+    let self = this
+    self.getHistory()
+    setInterval(() => {
+      self.getHistory()
+    }, 60 * 1000)
   }
 }
 </script>
@@ -62,42 +118,50 @@ export default {
     <el-table-column prop="fee" label="Fee" min-width="75" :formatter="feeFormatter"/>
     <el-table-column prop="address_from" label="From" :formatter="addressFormatter" min-width="120"/>
     <el-table-column prop="address_to" label="To" :formatter="addressFormatter" min-width="120"/>
-    <el-table-column prop="network" label="Network" min-width="100">
+    <el-table-column prop="network" label="Network" min-width="100" :filters="networkFilters"
+                     :filter-method="filterNetwork">
       <template #default="scope">
         <el-tag
             :color="colorMapping[scope.row.network]"
             disable-transitions
-        >{{ toUpperCase(scope.row.network) }}</el-tag
+        >{{ toUpperCase(scope.row.network) }}
+        </el-tag
         >
       </template>
     </el-table-column>
-    <el-table-column prop="tag" label="Tag" min-width="110">
+    <el-table-column prop="tag" label="Tag" min-width="110" :filters="tagFilters" :filter-method="filterTag">
       <template #default="scope">
         <el-tag
             :color="colorMapping[scope.row.tag]"
             disable-transitions
-        >{{ toUpperCase(scope.row.tag) }}</el-tag
+        >{{ toUpperCase(scope.row.tag) }}
+        </el-tag
         >
       </template>
     </el-table-column>
-    <el-table-column prop="type" label="Type" min-width="100">
+    <el-table-column prop="type" label="Type" min-width="100" :filters="typeFilters" :filter-method="filterType">
       <template #default="scope">
         <el-tag
             :color="colorMapping[scope.row.type]"
             disable-transitions
-        >{{ toUpperCase(scope.row.type) }}</el-tag
+        >{{ toUpperCase(scope.row.type) }}
+        </el-tag
         >
       </template>
     </el-table-column>
     <el-table-column fixed="right" label="Transaction Details" min-width="150">
-      <template #default>
-        <el-link link type="primary" size="small" @click="handleClick">OpenSea
-        </el-link>
-        <br/>
-        <el-link link type="primary" size="small" @click="handleClick">Etherscan
-        </el-link>
+      <template #default="scope">
+        <div v-for="url in scope.row.actions[0].related_urls">
+          <el-link link type="primary" size="small" :href="url" target="_blank">
+            {{ getDomain(url) }}
+          </el-link>
+          <br>
+        </div>
+
       </template>
     </el-table-column>
   </el-table>
-  <el-space direction="vertical" :size="100"><div></div></el-space>
+  <el-space direction="vertical" :size="100">
+    <div></div>
+  </el-space>
 </template>
